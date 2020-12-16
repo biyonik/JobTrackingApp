3d08a381c93d1371e51f6a37c22e06a4480c3701
Ahmet Altun <ahmet.altun60@hotmail.com>
2020-12-16 20:23:28 +0300

    UI tarafında Layoutlar oluşturuldu ve temanın ilk ayarlamaları yapıldı. Tema giydirme işlemlerine başlandı

---
+++ JobTrackingApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobTrackingApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ JobTrackingApp.WebUI/Middlewares/CustomStaticFileOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+namespace JobTrackingApp.WebUI.Middlewares
+{
+    public static class CustomStaticFileOptions
+    {
+        public static StaticFileOptions GetFileOptions(IApplicationBuilder app, IWebHostEnvironment env, string path)
+        {
+            string root = Path.Combine(env.ContentRootPath,  path);
+            return new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(root),
+                RequestPath = "/"+path
+            };
+        }
+    }
+}
